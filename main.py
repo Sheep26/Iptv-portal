14fa3ce06af179d1c9ddd7592be4967c79501d58
Sheep26 <crabeykiller@gmail.com>
2025-06-07 23:24:59 +1200

    Update main.py

--- main.py
+++ main.py
@@ -268,7 +268,6 @@ def web_server(arg):
     @app.route("/server/get_m3u")
     def get_m3u_all():
         search = request.args.get("search", None)
-        https = request.args.get("https", config["https"])
         file_content = "#EXTM3U"
         for server in servers:
             for channel in server.channels:
@@ -276,7 +275,7 @@ def web_server(arg):
                     if not search.lower() in channel["name"].lower():
                         continue
                 file_content += f"\n#EXTINF:-1 tvg-logo=\"{channel['logo']}\" group-title=\"{channel['name']}\",{channel['name']}"
-                file_content += f"\n{'https' if https else 'http'}://{request.url.split('/')[2].replace(':', '')}/play/{server.id}/{channel['id']}"
+                file_content += f"\n{'https' if config["https"] else 'http'}://{request.url.split('/')[2].replace(':', '')}/play/{server.id}/{channel['id']}"
         return Response(file_content, mimetype='text/plain')
 
     @app.route("/server/get_channels")
