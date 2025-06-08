ab2e56b0a17ef6803e48cc69353c337136c9dbae
Sheep26 <crabeykiller@gmail.com>
2025-06-08 18:54:02 +1200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 mkdir /etc/iptv
 
+apt install python3-flask
+apt install python3-requests
 mv main.py /etc/iptv/iptv
 mv config.json /etc/iptv/config.json
 mv iptv.service /etc/systemd/system/iptv.service
