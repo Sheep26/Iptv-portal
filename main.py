39801d7778fe18fb1688ba4ee944541fb38433c1
Sheep26 <crabeykiller@gmail.com>
2025-06-17 18:39:08 +1200

    Update main.py

--- main.py
+++ main.py
@@ -399,7 +399,7 @@ def web_server():
                     if not search.lower() in channel["name"].lower():
                         continue
                 file_content += f"\n#EXTINF:-1 tvg-logo=\"{channel['logo']}\" group-title=\"{channel['name']}\",{channel['name']}"
-                file_content += f"\n{'https' if config['https'] else 'http'}://{request.url.split('/')[2].replace(':', '')}/play/{server.id}/{channel['id']}?proxy={int(request.args.get('proxy', 1))}"
+                file_content += f"\n{'https' if config['https'] else 'http'}://{request.url.split('/')[2].replace(':', '')}/play/{server.id}/{channel['id']}?proxy={int(request.args.get('proxy', 0))}"
         return Response(file_content, mimetype='text/plain')
 
     @app.route("/server/get_channels")
@@ -498,7 +498,7 @@ def web_server():
                 if not search.lower() in channel["name"].lower():
                     continue
             file_content += f"\n#EXTINF:-1 tvg-logo=\"{channel['logo']}\" group-title=\"{channel['name']}\",{channel['name']}"
-            file_content += f"\n{request.url.split(':')[0]}://{request.url.split('/')[2].replace(':', '')}/play/{server}/{channel['id']}?proxy={int(request.args.get('proxy', 1))}"
+            file_content += f"\n{request.url.split(':')[0]}://{request.url.split('/')[2].replace(':', '')}/play/{server}/{channel['id']}?proxy={int(request.args.get('proxy', 0))}"
         
         return Response(file_content, mimetype='text/plain')
     
@@ -506,7 +506,7 @@ def web_server():
     def play(server, channel):
         if session.get("session_id", None) == None:
             session["session_id"] = rand_str(32)
-        return servers[int(server)].handle_play(channel, session["session_id"], int(request.args.get("proxy", 1)))
+        return servers[int(server)].handle_play(channel, session["session_id"], int(request.args.get("proxy", 0)))
     
     app.run("0.0.0.0", 8080)
 
