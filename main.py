e9272c4027fa57b0faaf0f4fedbdd7cfb7fbb414
Drew Fitzgerald <crabeykiller@gmail.com>
2026-02-21 22:25:07 +1300

    Update main.py

--- main.py
+++ main.py
@@ -432,7 +432,7 @@ def web_server():
                         break
                 if found: continue
                 
-                tvg_id = channel.get('epg_channel_id', f'{channel['name'].replace(' ', '').replace('(', '').replace(')', '')}.{server.id}.{channel['id']}')
+                tvg_id = channel.get('epg_channel_id', f"{channel['name'].replace(' ', '').replace('(', '').replace(')', '')}.{server.id}.{channel['id']}")
                 file_content += f"\n#EXTINF:-1 tvg-id=\"${tvg_id}\" tvg-logo=\"{channel['logo']}\" group-title=\"{channel['name']}\",{channel['name']}"
                 server_url = f"{'https' if config['https'] else 'http'}://{request.url.split('/')[2]}" if not config["proxy"] else config["proxy_url"]
                 stream_url = f"{server_url}/play/{server.id}/{channel['id']}?proxy={int(request.args.get('proxy', 0))}"
@@ -554,7 +554,7 @@ def web_server():
                     break
             if found: continue
             
-            tvg_id = channel.get('epg_channel_id', f'{channel['name'].replace(' ', '').replace('(', '').replace(')', '')}.{servers[int(server)].id}.{channel['id']}')
+            tvg_id = channel.get('epg_channel_id', f"{channel['name'].replace(' ', '').replace('(', '').replace(')', '')}.{servers[int(server)].id}.{channel['id']}")
             file_content += f"\n#EXTINF:-1 tvg-id=\"{tvg_id}\" tvg-logo=\"{channel['logo']}\" group-title=\"{channel['name']}\",{channel['name']}"
             server_url = f"{'https' if config['https'] else 'http'}://{request.url.split('/')[2]}" if not config["proxy"] else config["proxy_url"]
             stream_url = f"{server_url}/play/{servers[int(server)].id}/{channel['id']}"
