0ebc1aab4ad8c028bbc274d75ba92a04f1e3eac0
Sheep26 <crabeykiller@gmail.com>
2026-03-20 17:43:02 +1300

    No proxy if specified

--- main.py
+++ main.py
@@ -440,7 +440,7 @@ def web_server():
                     tvg_id = f"{channel['name'].replace(' ', '').replace('(', '').replace(')', '')}.{server.id}.{channel['id']}"
                 
                 file_content += f"\n#EXTINF:-1 tvg-id=\"{tvg_id}\" tvg-logo=\"{channel['logo']}\" group-title=\"{channel['name']}\",{channel['name']}"
-                server_url = f"{'https' if config['https'] else 'http'}://{request.url.split('/')[2]}" if not config["proxy"] else config["proxy_url"]
+                server_url = f"{'https' if config['https'] else 'http'}://{request.url.split('/')[2]}" if not config["proxy"] and not int(request.args['noproxy']) == 1 else config["proxy_url"]
                 stream_url = f"{server_url}/play/{server.id}/{channel['id']}?proxy={int(request.args.get('proxy', 0))}"
                 if original_links: file_content += f"\n{channel['url'] if type(server) == Server or type(server) == XtreamServer else stream_url}"
                 else: file_content += f"\n{stream_url}"
@@ -565,7 +565,7 @@ def web_server():
                 tvg_id = f"{channel['name'].replace(' ', '').replace('(', '').replace(')', '')}.{servers[int(server)].id}.{channel['id']}"
             
             file_content += f"\n#EXTINF:-1 tvg-id=\"{tvg_id}\" tvg-logo=\"{channel['logo']}\" group-title=\"{channel['name']}\",{channel['name']}"
-            server_url = f"{'https' if config['https'] else 'http'}://{request.url.split('/')[2]}" if not config["proxy"] else config["proxy_url"]
+            server_url = f"{'https' if config['https'] else 'http'}://{request.url.split('/')[2]}" if not config["proxy"] and not int(request.args['noproxy']) == 1 else config["proxy_url"]
             stream_url = f"{server_url}/play/{servers[int(server)].id}/{channel['id']}"
             if original_links: file_content += f"\n{channel['url'] if type(servers[int(server)]) == Server or type(servers[int(server)]) == XtreamServer else stream_url}"
             else: file_content += f"\n{stream_url}"
